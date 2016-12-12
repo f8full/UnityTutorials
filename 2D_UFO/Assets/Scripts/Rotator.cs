63d06a9502a7aed08c463bf3063396b437511321
F8Full <f8full@gmail.com>
2016-12-12 18:42:06 -0500

    Step 6 of 9
    - added pickups
    - added rotator script
    - created pickup prefab

---
+++ 2D_UFO/Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () { //instead of fixedUpdate because we don'T use forces
+        transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+	}
+}
