fdb31782c822fa6d54bdeaf4489d32d234372cb3
F8Full <f8full@gmail.com>
2016-12-12 18:26:14 -0500

    step 5 of 9
    - added camera controller

---
+++ 2D_UFO/Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private Vector3 mOffset;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        mOffset = transform.position - player.transform.position;
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        transform.position = player.transform.position + mOffset;
+
+    }
+}
