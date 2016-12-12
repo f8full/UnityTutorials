fe05bd3bf1e410fee0153129de32b0dfffdcb95c
F8Full <f8full@gmail.com>
2016-12-12 17:30:30 -0500

    Step 3 of 9
    - added rigidBody2D component
    - wrote input script

---
+++ 2D_UFO/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D mRigidBody2D;
+
+	// Use this for initialization
+	void Start () {
+
+        mRigidBody2D = GetComponent<Rigidbody2D>();
+		
+	}
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        mRigidBody2D.AddForce(new Vector2(moveHorizontal, moveVertical) * speed);
+    }
+}
