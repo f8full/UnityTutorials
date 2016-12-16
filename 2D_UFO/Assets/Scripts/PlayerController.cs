e1411c632790fc5cc7ed8e8fcba993a4a97b6a44
F8Full <f8full@gmail.com>
2016-12-15 20:26:30 -0500

    Step 8 of 9 - UI
    - added UI text elements for collectible count and win message
    - updated player controller script to update UI strings

--- 2D_UFO/Assets/Scripts/PlayerController.cs
+++ 2D_UFO/Assets/Scripts/PlayerController.cs
@@ -1,17 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+    public Text countTextUI;
+    public Text winTextUI;
+
+
 
     private Rigidbody2D mRigidBody2D;
+    private int mCollectibleCount;
 
 	// Use this for initialization
 	void Start () {
 
         mRigidBody2D = GetComponent<Rigidbody2D>();
+        mCollectibleCount = 0;
+        setCountText();
+        winTextUI.text = "";
+        
 		
 	}
 
@@ -29,6 +39,18 @@ public class PlayerController : MonoBehaviour {
         if(other.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+            ++mCollectibleCount;
+            setCountText();
+        }
+    }
+
+    void setCountText()
+    {
+        countTextUI.text = "Count: " + mCollectibleCount.ToString();
+
+        if(mCollectibleCount >= 10)
+        {
+            winTextUI.text = "You win !!";
         }
     }
 }
