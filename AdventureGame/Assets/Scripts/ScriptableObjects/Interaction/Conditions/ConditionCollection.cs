22c91cef144aa96bd6f5718bd58e918dd47227c3
F8Full <f8full@gmail.com>
2017-01-13 18:36:28 -0500

    Step 3 - Conditions
    - rewrote runtime script (was already there because of importation of
    assets for step 2)

--- AdventureGame/Assets/Scripts/ScriptableObjects/Interaction/Conditions/ConditionCollection.cs
+++ AdventureGame/Assets/Scripts/ScriptableObjects/Interaction/Conditions/ConditionCollection.cs
@@ -3,20 +3,24 @@
 public class ConditionCollection : ScriptableObject
 {
     public string description;
+
     public Condition[] requiredConditions = new Condition[0];
     public ReactionCollection reactionCollection;
 
-
     public bool CheckAndReact()
     {
-        for (int i = 0; i < requiredConditions.Length; i++)
+        for(int i=0; i < requiredConditions.Length; ++i)
         {
-            if (!AllConditions.CheckCondition (requiredConditions[i]))
+            if (!AllConditions.CheckCondition(requiredConditions[i]))
+            {
                 return false;
+            }
         }
 
-        if(reactionCollection)
+        if (reactionCollection)
+        {
             reactionCollection.React();
+        }
 
         return true;
     }
